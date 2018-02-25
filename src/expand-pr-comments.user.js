5a22708ec154376e054d635cc95bb387536cd7e8
Sky JIA <me@skyjia.com>
2018-02-25 15:05:36 +0800

    Update expand-pr-comments.user.js

--- src/expand-pr-comments.user.js
+++ src/expand-pr-comments.user.js
@@ -4,7 +4,7 @@
 // @author       skyjia
 // @oujs:author  skyjia
 // @namespace    https://github.com/skyjia
-// @version      0.1.1
+// @version      0.1.2
 // @homepageURL  https://github.com/skyjia/expand-pr-comments/
 // @supportURL   https://github.com/skyjia/expand-pr-comments/issues
 // @include      /^https://github\.com/(\w|-)+/(\w|-)+/pull/\d+/
