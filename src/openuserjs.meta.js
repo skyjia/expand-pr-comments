38754504a341ab93d84627af9e8eb116f7b3c053
Sky JIA <me@skyjia.com>
2018-09-25 14:47:05 +0800

    Create openuserjs.meta.js

---
+++ src/openuserjs.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Expand All PR Comments
+// @description  Expand all GitHub PR comments including outdated ones.
+// @author       skyjia
+// @oujs:author  skyjia
+// @namespace    https://github.com/skyjia
+// @version      0.1.3
+// @homepageURL  https://github.com/skyjia/expand-pr-comments/
+// @supportURL   https://github.com/skyjia/expand-pr-comments/issues
+// @include      /^https://github\.com/(\w|-)+/(\w|-)+/pull/\d+/
+// @run-at       document-body
+// @grant        none
+// @license MIT
+// ==/UserScript==
