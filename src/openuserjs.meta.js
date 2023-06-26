b79bf44874909a08eba7bcb07c1b41b8a61b53a2
CeruleanSky <CeruleanSky@users.noreply.github.com>
2023-06-26 10:53:39 -0400

    Update for Github 2023

--- src/openuserjs.meta.js
+++ src/openuserjs.meta.js
@@ -4,11 +4,11 @@
 // @author       skyjia
 // @oujs:author  skyjia
 // @namespace    https://github.com/skyjia
-// @version      0.1.3
+// @version      0.1.4
 // @homepageURL  https://github.com/skyjia/expand-pr-comments/
 // @supportURL   https://github.com/skyjia/expand-pr-comments/issues
 // @updateURL    https://raw.githubusercontent.com/skyjia/expand-pr-comments/master/src/openuserjs.meta.js
-// @include      /^https://github\.com/(\w|-)+/(\w|-)+/pull/\d+/
+// @include      /^https://github\.com/(\w|-)+/(\w|-)+/(pull|issues)/\d+/
 // @run-at       document-body
 // @grant        none
 // @license MIT
